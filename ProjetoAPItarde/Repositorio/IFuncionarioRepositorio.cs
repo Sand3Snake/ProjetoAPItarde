dc9525bec4b3716ee24e3eb5afc7c1f07899d2e8
Sand3Snake <soaressuelen057@gmail.com>
2024-09-27 17:43:07 -0300

    update

---
+++ ProjetoAPItarde/Repositorio/IFuncionarioRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using ProjetoAPItarde.Model;
+
+namespace ProjetoAPItarde.Repositorio
+{
+    public interface IFuncionarioRepositorio
+    {
+        void Add(Funcionario funcionario, IFormFile foto);
+
+        List<Funcionario> GetAll();
+
+        public Funcionario GetById(int id);
+
+        void Update(Funcionario funcionario, IFormFile foto);
+
+        void Delete(int id);
+       
+    }
+}
